09a0e72b4653898e3452a87281f78d7fc995cf63
MikeyStrauss <44716697+MikeyStrauss@users.noreply.github.com>
2021-11-09 02:15:56 -0800

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ from dotenv.main import load_dotenv
 load_dotenv()
 
 # Discord config
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN", "")
-BOT_PREFIX = "?"
+DISCORD_TOKEN = os.getenv("OTA3NDUzNDc0OTkwNTk2MTk5.YYnZ6A.feC_dC6S24I8DjyyDFEshti9mUM", "")
+BOT_PREFIX = "`"
